8fce3951dd91b98eacf0b8a31c4cc15a3ef080e8
Bohdan bc2024-4 <marchukbohdan29@gmail.com>
2024-10-11 22:48:17 +0300

    Added basic frontend for main page

--- client/src/pages/MainPage/MainPage.jsx
+++ client/src/pages/MainPage/MainPage.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
 
+import "./MainPage.scss"
+
 const MainPage = () => {
     return (
-        <div>
-            <h1>Main page</h1>
-            <h2> gheffdjas</h2>
+        <div className="container">
+            <div className="main-page">
+                <h4>Додати подію</h4>
+                <form className="form form login">
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <input 
+                                type="text" 
+                                id="text"
+                                name="input"
+                                className="validate"
+                            />
+                            <label htmlFor="input">Подія: </label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <button className="waves-effect waves-light btn green">Додати</button>
+                    </div>
+                </form>
+
+                <h3>Створені події</h3>
+                <div className="events">
+                    <div className="row flex events-item">
+                        <div className="col events-num">1</div>
+                        <div className="col events-text">Text</div>
+                        <div className="col events-buttons">
+                            <i className="material-icons blue-text">check</i>
+                            <i className="material-icons orange-text">warning</i>
+                            <i className="material-icons red-text">delete</i>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
